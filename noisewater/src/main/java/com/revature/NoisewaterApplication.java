1117ff4cee08a295f44eb5491864d5aaaa60be15
Mark Kolb <99706832+TheToasterRoaster@users.noreply.github.com>
2022-03-22 20:44:38 -0400

    testing

---
+++ noisewater/src/main/java/com/revature/NoisewaterApplication.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoisewaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoisewaterApplication.class, args);
+	}
+
+}
