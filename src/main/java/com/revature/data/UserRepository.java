2c35801caf6c42185a3f5f4f924eab613e1b74b4
Mark Kolb <99706832+TheToasterRoaster@users.noreply.github.com>
2022-03-25 14:55:19 -0400

    many to many

--- src/main/java/com/revature/data/UserRepository.java
+++ src/main/java/com/revature/data/UserRepository.java
@@ -1,5 +1,10 @@
 package com.revature.data;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface UserRepository extends JpaRepository {
+
+	
 }
