e89818cf316625241b85e0f05b5f9d7f881ee69d
Mark Kolb <99706832+TheToasterRoaster@users.noreply.github.com>
2022-03-29 14:41:03 -0400

    Updated Models

--- src/main/java/com/revature/models/Song.java
+++ src/main/java/com/revature/models/Song.java
@@ -11,13 +11,20 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.ToString;
 
 @Data @AllArgsConstructor @NoArgsConstructor
 @Entity 
+@JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="id")
+@EqualsAndHashCode(exclude={"owners"}) @ToString(exclude= {"owners"})
 public class Song {
 	
 	@Id
@@ -34,4 +41,9 @@ public class Song {
 	@ManyToMany(mappedBy="songs", fetch = FetchType.EAGER, cascade = CascadeType.PERSIST) // declare the owner of the relationship by mapping it to the property of the User class
 	private @NonNull Set<User> owners;
 
+	public void addUser(User user) {
+        owners.add(user);
+        user.getSongs().add(this);
+    }
+	
 }
--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -9,21 +9,24 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.JoinColumn;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-// import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data @AllArgsConstructor @NoArgsConstructor
+@JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="id")
 @Entity
 @Table(name="users")
 public class User {
